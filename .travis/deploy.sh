93575c1028df523c20b89a07eb8cc7e9a64dcf62
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-03-20 20:12:21 +0100

    Copy CI artifacts to the right destination

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -41,4 +41,4 @@ openssl aes-256-cbc -K $encrypted_310baf0cb798_key -iv $encrypted_310baf0cb798_i
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_rsa
 ssh-add /tmp/deploy_rsa
-scp $filename $DEPLOY_USER@$DEPLOY_HOST:$TRAVIS_BRANCH
+scp $filename $DEPLOY_USER@$DEPLOY_HOST:$TRAVIS_BRANCH/$filename
