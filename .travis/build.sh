d39ecfb2d3dce90a17e48edb3cb0baee9aca2667
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-11-02 08:00:48 +0100

    Use Fedora for the CI
    KDE Neon on 16.04 is EOL, better migrate to a supported distribution.

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,21 +4,12 @@ set -e
 
 source /usr/local/share/liri-travis/functions
 
-# Install libqtxdg
-# It uses Qt private API but it's not built by KDE Neon CI therefore it
-# still needs ABI of the original Ubuntu packages and doesn't work
-travis_start "build_libqtxdg"
-msg "Build libqtxdg..."
-pushd /usr/src
-git clone -b 3.1.0 git://github.com/lxqt/libqtxdg.git
-cd libqtxdg
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
-make -j$(nproc)
-sudo make install
-popd
-travis_end "build_libqtxdg"
+# Install dependencies
+travis_start "install_packages"
+msg "Install packages..."
+dnf install -y \
+    libqtxdg-devel
+travis_end "install_packages"
 
 # Install artifacts
 travis_start "artifacts"
@@ -30,7 +21,7 @@ travis_end "artifacts"
 travis_start "qbs_setup"
 msg "Setup qbs..."
 qbs setup-toolchains --detect
-qbs setup-qt $(which qmake) travis-qt5
+qbs setup-qt $(which qmake-qt5) travis-qt5
 qbs config profiles.travis-qt5.baseProfile $CC
 travis_end "qbs_setup"
 
@@ -41,7 +32,7 @@ dbus-run-session -- \
 xvfb-run -a -s "-screen 0 800x600x24" \
 qbs -d build -j $(nproc) --all-products profile:travis-qt5 \
     modules.lirideployment.prefix:/usr \
-    modules.lirideployment.libDir:/usr/lib/x86_64-linux-gnu \
-    modules.lirideployment.qmlDir:/usr/lib/x86_64-linux-gnu/qt5/qml \
-    modules.lirideployment.pluginsDir:/usr/lib/x86_64-linux-gnu/qt5/plugins
+    modules.lirideployment.libDir:/usr/lib64 \
+    modules.lirideployment.qmlDir:/usr/lib64/qt5/qml \
+    modules.lirideployment.pluginsDir:/usr/lib64/qt5/plugins
 travis_end "build"
